705c6e8a63e4a7ec03708fbbc3e6ce83b04c5437
Luis Gustavo Cardoso Zanetti Pereira <49000286+LuisGustavoCZP@users.noreply.github.com>
2022-02-10 19:53:41 -0300

    m

--- Aula3/main.js
+++ Aula3/main.js
@@ -1,10 +1,12 @@
 const key = `AIzaSyDAj4mG_DKWUJgK4Pv0BB5eUj9PQ-aMXT8`;
-const map = document.getElementById("gmap");
+const map = $("#gmap");
 let mtype = "satellite";
-let zoom = 18;
+let zoom = 16;
 let cep = "";
+const url = "https://www.google.com/maps?api=1";
 
-function CEPChange (e) 
+///search/
+function CEPChange (e)
 {
   cep = e.target.value;
   console.log(cep);
@@ -17,7 +19,16 @@ function CEPLocal ()
   .done(data => 
   {
     console.log(data);
-    map.src = `https://www.google.com/maps/embed/v1/view?key=${key}&center=${data.lat},${data.lng}&zoom=${zoom}&maptype=${mtype}`;
+    $("#rua").html(`<span class="p">${data.address_type} </span> ${data.address_name}`);
+    $("#bairro").html(`<span class="p">Bairro</span> ${data.district}`);
+    $("#cidade").html(`<span class="p">Cidade</span> ${data.city}`);
+    $("#ddd").html(`<span class="p">DDD</span> ${data.ddd}`);
+    $("#ibge").html(`<span class="p">IBGE</span> ${data.city_ibge}`);
+    $("#lat").html(`<span class="p">LAT</span> ${data.lat}`);
+    $("#long").html(`<span class="p">LONG</span> ${data.lng}`);
+   
+    const t0 = `${url}&q=${data.lat},${data.lng}+(CEP%20${data.cep})&output=embed`;
+    map.attr('src',t0); ///?api=1&zoom=${zoom}&maptype=${mtype}
   });
 }
 
