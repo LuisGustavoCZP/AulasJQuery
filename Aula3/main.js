9fd612dc987482b4630e127fc83946d22e80ee94
Luis Gustavo Cardoso Zanetti Pereira <49000286+LuisGustavoCZP@users.noreply.github.com>
2022-02-11 10:17:29 -0300

    m

--- Aula3/main.js
+++ Aula3/main.js
@@ -9,16 +9,46 @@ const url = "https://www.google.com/maps?api=1";
 function CEPChange (e)
 {
   cep = e.target.value;
+  if(cep.length != 8) 
+  {
+    e.target.classList.add("r");
+  } 
+  else 
+  {
+    e.target.classList.remove("r");
+  }
+  
   console.log(cep);
+}
+
+function ClearFields (){
+  $("#rua").html("");
+  $("#bairro").html("");
+  $("#cidade").html("");
+  $("#ddd").html("");
+  $("#ibge").html("");
+  $("#lat").html("");
+  $("#long").html("");
+  map.attr('src', "");
 }
 
 function CEPLocal ()
 {
-  if(cep.length != 8) return;
+  const result = $("#result");
+  if(cep.length != 8) {
+    result.addClass("r");
+    result.html("Indique um CEP com 8 digitos!");
+    ClearFields ();
+    return;
+  }
+
   $.ajax({url:`https://cep.awesomeapi.com.br/json/${cep}`})
   .done(data => 
   {
     console.log(data);
+    result.html("O CEP indicado foi encontrado");
+    result.removeClass("r");
+    
     $("#rua").html(`<span class="p">${data.address_type} </span> ${data.address_name}`);
     $("#bairro").html(`<span class="p">Bairro</span> ${data.district}`);
     $("#cidade").html(`<span class="p">Cidade</span> ${data.city}`);
@@ -29,8 +59,13 @@ function CEPLocal ()
    
     const t0 = `${url}&q=${data.lat},${data.lng}+(CEP%20${data.cep})&output=embed`;
     map.attr('src',t0); ///?api=1&zoom=${zoom}&maptype=${mtype}
+  }).fail(function (jqXHR, textStatus) {
+    result.addClass("r");
+    result.html("O CEP indicado não foi encontrado!")
+    ClearFields ();
   });
 }
 
-$("#CEP").on("input", CEPChange);
+//$("#CEP").on("input", CEPChange);
+$("#CEP").on("keyup", CEPChange);
 $("#search").on("click", CEPLocal);
